7f2c776a2087df09e43e7e927ec12869bc569977
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 20:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {
   key: 'value'
 }
-function updateObjectWithKeyAndValue(recipes, key, value) {
+function updateObjectWithKeyAndValue(recipes, key2, value2) {
   recipes[key2] = 'value2'
   return recipes
 }
