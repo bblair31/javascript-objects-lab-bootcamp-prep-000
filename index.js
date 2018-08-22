f416326a835a3b1932ea8746c8012e4530d7b3ac
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 20:13:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {
   prop: '1'
 }
-function updateObjectWithKeyAndValue(recipes, prop2, value2) {
+function updateObjectWithKeyAndValue(recipes, prop, value) {
   recipes[prop2] = '2'
   return recipes
 }
